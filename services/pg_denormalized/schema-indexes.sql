f5b6de7833478379f0bee5edd43013867a9ec381
Jay Maliye <jaymaliye@gmail.com>
2023-05-02 20:56:23 -0700

    update

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -1,10 +1,8 @@
 CREATE INDEX idx_jsonb_1 ON tweets_jsonb USING gin((data->'entities'->'hashtags'));
-
 CREATE INDEX idx_jsonb_2 ON tweets_jsonb USING gin((data->'extended_tweet'->'entities'->'hashtags'));
-
 CREATE INDEX idx_jsonb_3 ON tweets_jsonb USING gin((data->'lang'));
-
+-- e
 CREATE INDEX idx_jsonb_4 ON tweets_jsonb USING gin(to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text'))) WHERE data->>'lang'='en';
-
+-- d
 CREATE INDEX idx_jsonb_5 ON tweets_jsonb USING gin(to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text')),
     (data->'entities'->'hashtags' || COALESCE(data->'extended_tweet'->'entities'->'hashtags','[]')));
--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,8 +1,5 @@
-CREATE INDEX idx_id_tweets_tag ON tweet_tags (id_tweets, tag);
-
 CREATE INDEX idx_tag_id_tweets ON tweet_tags (tag, id_tweets);
-
+CREATE INDEX idx_id_tweets_tag ON tweet_tags (id_tweets, tag);
 CREATE INDEX idx_id_tweets_lang ON tweets (id_tweets, lang);
-
 CREATE INDEX idx_gin ON tweets USING gin(to_tsvector('english',text)) WHERE lang='en';
 
